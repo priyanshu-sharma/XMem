cf6e9cae38081a02a47c9b97535cbdf9221e9731
priyanshu-sharma <prynsh1112@gmail.com>
2023-06-13 16:58:00 -0700

    hf errror

--- util/logger.py
+++ util/logger.py
@@ -55,7 +55,7 @@ class TensorboardLogger:
     def log_metrics(self, l1_tag, l2_tag, val, step, f=None):
         tag = l1_tag + '/' + l2_tag
         text = '{:s} - It {:6d} [{:5s}] [{:13}]: {:s}'.format(self.short_id, step, l1_tag.upper(), l2_tag, fix_width_trunc(val))
-        wandb.log(l2_tag, val)
+        wandb.log({l2_tag: val})
         print(text)
         print("Result - {}\n".format(text))
         if f is not None:

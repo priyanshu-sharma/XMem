c42232f852657e0e2f972471f192665f2e53b01a
priyanshu-sharma <prynsh1112@gmail.com>
2023-06-13 16:54:58 -0700

    add wandb logger

--- util/logger.py
+++ util/logger.py
@@ -7,6 +7,8 @@ import warnings
 
 import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter
+import wandb
+wandb.init(project='XMem Training', id='Training Stage - 0')
 
 
 def tensor_to_numpy(image):
@@ -53,6 +55,7 @@ class TensorboardLogger:
     def log_metrics(self, l1_tag, l2_tag, val, step, f=None):
         tag = l1_tag + '/' + l2_tag
         text = '{:s} - It {:6d} [{:5s}] [{:13}]: {:s}'.format(self.short_id, step, l1_tag.upper(), l2_tag, fix_width_trunc(val))
+        wandb.log(l2_tag, val)
         print(text)
         print("Result - {}\n".format(text))
         if f is not None:
